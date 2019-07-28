c4f72a50e386ad1cdc6912e9b0ae3e2802509785
Pavel Kazadoi <paulnightjar@gmail.com>
2019-07-28 20:22:55 +0200

    Csv export

--- src/Repository/EmployeesRepository.php
+++ src/Repository/EmployeesRepository.php
@@ -19,5 +19,23 @@ class EmployeesRepository extends ServiceEntityRepository
         parent::__construct($registry, Employees::class);
     }
 
+    /**
+     * Get employees information
+     * @return array
+     */
+    public function getExport(): array
+    {
+        $conn = $this->getEntityManager()->getConnection();
+
+        $sql = 'SELECT e.employee, t.name, e.distance, c.value, c.date
+                FROM employees e
+                LEFT JOIN compensation c on e.id = c.employee_id
+                LEFT JOIN transport t on e.transport_id = t.id
+                ORDER BY c.date ASC, e.id ASC';
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+
+        return $stmt->fetchAll();
+    }
 
 }
